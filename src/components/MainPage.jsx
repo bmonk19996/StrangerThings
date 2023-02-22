efaaa5cee4df2c837d75e4246ace954e2dfd5cc2
Your Name <parker.josie.eden@gmail.com>
2023-02-22 12:49:28 -0600

    search filter is working correctly, just need to display

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,6 +5,27 @@ import { getPosts } from "../API-Adapt";
 export default function MainPage() {
   const [token,, showNew,] = useOutletContext();
   const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+useEffect(() => {
+
+  const temporary = posts.filter(post => 
+    (post.title).includes(searchTerm)
+  );
+
+  setFilteredPosts(temporary);
+
+}, [searchTerm])
+
+function filterPosts(post){
+  if((post.title).includes(searchTerm)){
+    // console.log(post);
+    return post;
+  }
+}
+
+console.log(filteredPosts);
 
   async function retrievePosts() {
     const myPosts = await getPosts(token);
@@ -15,6 +36,12 @@ export default function MainPage() {
   }, [token]);
   return (
     <div className="mainPage">
+      <div id="searchBar">
+        <form>
+          <label>Search for Posts:</label>
+          <input type='text' onInput={(event) => {setSearchTerm(event.target.value)}}></input>
+        </form>
+      </div>
       <PostList posts={posts} setPosts={setPosts} token={token} />
       {token && showNew ? (
         <NewPost token={token} setPosts={setPosts} posts={posts} />
